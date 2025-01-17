5a286c5ac12bf9e5dee27a8b1cf67765fa56d671
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-18 00:32:50 +0100

    Switch-Case has been reviewed

---
+++ CSharp_SwitchCase/Program.cs
@@ -0,0 +1,23 @@
+﻿
+#region Switch-Case
+
+//int x = 10;
+//switch (x)
+//{
+//    case 1:
+//        Console.WriteLine("x is 1");
+//        break;
+//    case 2:
+//        Console.WriteLine("x is 2");
+//        break;
+//    case 3:
+//        Console.WriteLine("x is 3");
+//        break;
+//    default:
+//        Console.WriteLine("x is not 1, 2 or 3");
+//        break;
+//}
+
+// basically switch-case is used to compare a variable with multiple values
+
+#endregion
