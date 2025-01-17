6fe23dffa0d3073f52782fa32a4e1314283384c6
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-18 00:23:49 +0100

    Reviewed C# Operators and changed the solution name

--- CSharp_Operators/Program.cs
+++ CSharp_Operators/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Data Types
+ * Stack - Heap
+ * Boxing - Unboxing
+ * Explicit - Implicit Conversion
+ * Checked - Unchecked
+*/
+
+/* Operators
+ * Arithmetic Operators
+ * Increment and Decrement Operators
+ * Assignment Operator
+ * Comparison Operators
+ * Logical Operators
+ */
+
+#region Arithmetic Operators
+
+// +, -, *, /, %
+
+//int x = 5;
+//double y = 6;
+//double result = x * y; // cannot be int because int * double = double
+//result = 100 % 5; // 0 
+#endregion
+
+#region Increment - Decrement Operators
+
+// ++, --
+
+//int x = 15;
+// x = x - 1;
+//x--; // 14
+//x++; // 15
+
+//--x; // 14
+
+//int y = 10;
+//int z = 5;
+//Console.WriteLine(y++); // output 11
+//Console.WriteLine(++z); // output 5 because it first shows the value then increments
+
+
+#endregion
+
+#region Assignment Operator
+
+//int x = 5; // "=" assigns 5 to x
+#endregion
+
+#region Comparison Operators
+// ==, !=, >, <, >=, <=
+
+//int x = 15;
+//int y = 32;
+
+//bool result = x > y; // false, it compares the values
+//Console.WriteLine(result);
+
+#endregion
+
+#region Logical Operators
+
+// &&, ||, !
+#endregion
