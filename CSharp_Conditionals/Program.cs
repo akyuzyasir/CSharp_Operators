6e9fd9ecdd50de57aad60633e70fb98f2cf18a87
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-18 00:40:15 +0100

    Conditionals has been reviewed

---
+++ CSharp_Conditionals/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//CONDITIONALS
+#region if-else
+
+//int x = 5;
+//int y = 12;
+
+//if(x > 14)
+//{
+//    // if the condition is true , this block of code will be executed
+//}
+//else
+//{
+//    // if the condition is false , this block of code will be executed
+//}
+
+//bool isWinter = true;
+
+//if (!isWinter)
+//{
+//    Console.WriteLine("No need for winter tires");
+//}
+//else
+//{
+//    Console.WriteLine("You need winter tires");
+//}
+
+#endregion
+
+#region if-elseif-else
+
+//int x = 10;
+//int y = 20;
+
+//// in case of multiple conditions , we use else if structure
+
+//if (x > y)
+//{
+//    Console.WriteLine("x greater than y");
+//}
+//else if (x < y)
+//{
+//    Console.WriteLine("x less than y");
+//}
+//else
+//{
+//    Console.WriteLine("x equals y");
+//}
+
+
+//// we don't have to use scopes if we have only one line of code in the block
+//if(true)
+//    Console.WriteLine("");
+
+#endregion
