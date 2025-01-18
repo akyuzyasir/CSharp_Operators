492a55224cd72d45f31bf114dfdb105caa3c96cd
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-01-18 15:31:33 +0100

    Foreach has been reviewed

---
+++ CSharp_08_ForEach/Program.cs
@@ -0,0 +1,8 @@
+﻿// we use foreach loop to iterate over a collection of items. The collection can be an array, list, dictionary, etc.
+
+int[] numbers = new int[] { 5, 33, 1, 7, 31, 73, 31, 98 };
+
+foreach(var number in numbers)
+{
+    Console.WriteLine("Ackuse Academy");
+}
